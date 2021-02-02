fcc1e8109169bc56b045b869b6bf1b1eb1677e5d
Mahadi Hassan <itsmahadi@gmail.com>
2021-02-02 15:56:50 +0600

    update model

--- DRFpractice/DRFpractice/settings.py
+++ DRFpractice/DRFpractice/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'basic_api.apps.BasicApiConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
